85629e091ddff4982e14a61d9bab123678cdd8b5
Bladedragonx <ticksobrinho@hotmail.com>
2022-01-27 16:38:32 -0300

    Atualização do Site

--- js/cards.js
+++ js/cards.js
@@ -1,9 +1,17 @@
-var dadosjSon = '';
-$(document).ready(function(){
-	$.get( "data/digimon.json", function(data) {
-        for(var i = 0; i < data.length; i++){
-            dadosjSon += '<a href="digipedia.html?digimon='+data[i].id+'"><div class="card"><img src="https://digimon.shadowsmith.com/img/'+ data[i].Name.toLowerCase() + '.jpg" alt="'+data[i].Name+'"><p class="digimon-id">N: '+ data[i].id +'</p><p class="digimon-name">'+ data[i].Name +'</p></div></a>';
-        }
-        $('.card-deck').html(dadosjSon);
-	});
-});
+var startcard = 0;
+function deckLoad(cardvalue){
+    var dadosjSon = $('.card-deck').html();
+    $(document).ready(function(){
+        $.get( "data/digimon.json", function(data) {
+            console.log(data.length);
+            if(startcard + cardvalue > data.length) {
+                cardvalue = data.length - startcard;
+            }
+            for(var i = 0; i < cardvalue; i++){
+                dadosjSon += '<a href="digipedia.html?digimon='+data[i+startcard].id+'"><div class="card"><img src="https://digimon.shadowsmith.com/img/'+ data[i+startcard].Name.toLowerCase() + '.jpg" alt="'+data[i+startcard].Name+'"><p class="digimon-id">N: '+ data[i+startcard].id +'</p><p class="digimon-name">'+ data[i+startcard].Name +'</p></div></a>';
+            }
+            startcard += cardvalue
+            $('.card-deck').html(dadosjSon);
+        });
+    });
+}
--- js/digipedia.js
+++ js/digipedia.js
@@ -7,14 +7,34 @@ $(document).ready(function(){
         if(myParam-1 >= 0){
             statsdigimon += '<div class="digimon-nameid"><p>'+ data[myParam-1].Name + ' N:' + data[myParam-1].id +'</p></div>';
             statsdigimon += '<div class="digimon-img"><img src="https://digimon.shadowsmith.com/img/'+ data[myParam-1].Name.toLowerCase() + '.jpg" alt="'+data[myParam-1].Name+'"></div>';
-            digivolutions += '<li class="first-evolution"><p><a href="digipedia.html?digimon='+data[myParam-1].id+'"><img src="https://digimon.shadowsmith.com/img/'+ data[myParam-1].Name.toLowerCase() + '.jpg" alt="'+data[myParam-1].Name+'"><br>'+ data[myParam-1].Name + ' N:' + data[myParam-1].id +'</a></p></li>'
-            digivolutions += '<ul class="second-evolution">'
-            for(let value of data[myParam-1].Digivolutions){
-                digivolutions += '<li><a href="digipedia.html?digimon='+data[value-1].id+'"><img src="https://digimon.shadowsmith.com/img/'+ data[value-1].Name.toLowerCase() + '.jpg" alt="'+data[value-1].Name+'"></a><p>'+ data[value-1].Name + ' N:' + data[value-1].id +'</p></li>'
+            digivolutions += '<ul class="'+data[myParam-1].Level+'">';
+            digivolutions += '<li><p><a href="digipedia.html?digimon='+data[myParam-1].id+'"><img src="https://digimon.shadowsmith.com/img/'+ data[myParam-1].Name.toLowerCase() + '.jpg" alt="'+data[myParam-1].Name+'"><br>'+ data[myParam-1].Name + ' N:' + data[myParam-1].id +'</a></p></li>';
+            digivolutions += '</ul>';
+            console.log(data[myParam-1].Digivolutions.length);
+            if(data[myParam-1].Digivolutions.length > 0){
+                digivolutions += '<span class="material-icons-outlined">arrow_forward_ios</span>';
+                switch(data[myParam-1].Level){
+                    case 'Fresh' && 'inTraining':
+                        digivolutions += '<ul class="Rookie">';
+                        break;
+                    case 'Rookie':
+                        digivolutions += '<ul class="Champion">';
+                        break;
+                    case 'Champion':
+                        digivolutions += '<ul class="Ultimate">';
+                        break;
+                    case 'Ultimate':
+                        digivolutions += '<ul class="Mega">';
+                        break;
+                }
+                for(let value of data[myParam-1].Digivolutions){
+                    digivolutions += '<li><a href="digipedia.html?digimon='+data[value-1].id+'"><img src="https://digimon.shadowsmith.com/img/'+ data[value-1].Name.toLowerCase() + '.jpg" alt="'+data[value-1].Name+'"></a><p>'+ data[value-1].Name + ' N:' + data[value-1].id +'</p></li>';
+                }
+                digivolutions += '</ul>';
             }
-            digivolutions += '</ul>'
+            digivolutions += '</ul>';
             $('.digigmon-stats').html(statsdigimon);
-            $('.evolution').html(digivolutions);
+            $('.digivolutions').html(digivolutions);
         }else{
             alert("Digimon não Encontrado");
         }
